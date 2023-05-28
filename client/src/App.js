c51357d0de51ead937801ab5eff6d399ae532d33
HecFer5 <hecalfer@hotmail.com>
2023-05-28 11:21:37 -0300

    segundo commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [nombre, setNombre] = useState('')
+const [edad, setEdad] = useState(0)
+const [pais, setPais] = useState('')
+const [cargo, setCargo] = useState('')
+const [experiencia, setExperiencia] = useState(0)
+
   return (
     <div className="App">
-    
+    <div className='datos'>
+      <label>Nombre: <input
+      onChange={(event)=>{
+        setNombre (event.target.value)
+      }}
+      type='text' /></label>
+
+      <label>Edad: <input
+       onChange={(event)=>{
+         setEdad (event.target.value)
+       }}
+      type='text' /></label>
+
+
+      <label>País: <input
+       onChange={(event)=>{
+        setPais (event.target.value)
+      }}
+      type='text' /></label>
+
+      <label>Cargo: <input
+       onChange={(event)=>{
+        setCargo (event.target.value)
+      }}
+      type='text' /></label>
+
+      <label>Experiencia: <input
+       onChange={(event)=>{
+        setExperiencia (event.target.value)
+      }}
+      type='number' /></label>
+      <button>Registrar</button>
+
+    </div>
     </div>
   );
 }
