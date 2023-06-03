b0727d3ce14cfa8132b437ba44b8816f1721a493
HecFer5 <hecalfer@hotmail.com>
2023-06-03 10:11:35 -0300

    3de junio

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,10 @@ function App() {
   const [pais, setPais] = useState('');
   const [cargo, setCargo] = useState('');
   const [experiencia, setExperiencia] = useState(0);
+  const [id, setId] = useState(0);
 
   const [empleados, setEmpleados] = useState([]);
+  const [editar, setEditar] = useState(false);
 
   ////////////////////////////// AGREGAR EMPLEADOS A LA TABLA
   const add = () => {
@@ -26,6 +28,17 @@ function App() {
       alert('Empleado registrado');
       getEmpleados();
     });
+  }
+
+  const editarEmpleado = (val) => {
+    setEditar(true);
+
+    setNombre(val.nombre);
+    setCargo(val.cargo);
+    setEdad(val.edad);
+    setPais(val.pais);
+    setExperiencia(val.experiencia);
+    setId(val.id);
   }
   /////// FIN AGREGAR
 
@@ -55,41 +68,42 @@ function App() {
               onChange={(event) => {
                 setNombre(event.target.value)
               }}
-              className="form-control" placeholder="Apellido y nombre" aria-label="Username" aria-describedby="basic-addon1" />
+              className="form-control" value={nombre} placeholder="Apellido y nombre" aria-label="Username" aria-describedby="basic-addon1" />
             <div />
             <span className="input-group-text" id="basic-addon1">Edad: </span>
             <input type="number"
               onChange={(event) => {
                 setEdad(event.target.value)
               }}
-              className="form-control" placeholder="Edad" aria-label="Username" aria-describedby="basic-addon1" />
+              className="form-control" value={edad} placeholder="Edad" aria-label="Username" aria-describedby="basic-addon1" />
             <div />
             <span className="input-group-text" id="basic-addon1">País: </span>
             <input type="text"
               onChange={(event) => {
                 setPais(event.target.value)
               }}
-              className="form-control" placeholder="País" aria-label="Username" aria-describedby="basic-addon1" />
+              className="form-control" value={pais} placeholder="País" aria-label="Username" aria-describedby="basic-addon1" />
             <div />
             <span className="input-group-text" id="basic-addon1">Cargo: </span>
             <input type="text"
               onChange={(event) => {
                 setCargo(event.target.value)
               }}
-              className="form-control" placeholder="Cargo" aria-label="Username" aria-describedby="basic-addon1" />
+              className="form-control" value={cargo} placeholder="Cargo" aria-label="Username" aria-describedby="basic-addon1" />
             <div />
             <span className="input-group-text" id="basic-addon1">Experiencia: </span>
             <input type="number"
               onChange={(event) => {
                 setExperiencia(event.target.value)
               }}
-              className="form-control" placeholder="Experiencia" aria-label="Username" aria-describedby="basic-addon1" />
+              className="form-control" value={experiencia} placeholder="Experiencia" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
         </div>
         <div className="card-footer text-body-secondary">
-          <button className='btn btn-primary' onClick={add}>Registrar</button>
-          <button className='btn btn-primary' onClick={getEmpleados}>Listar</button>
-
+          <div className="btn-group" role="group" aria-label="Basic example">
+            <button type="button" className="btn btn-info" onClick={add}>Ingresar</button>
+            <button type="button" className="btn btn-danger" onClick={getEmpleados}>Listar</button>
+          </div>
         </div>
       </div>
 
@@ -102,17 +116,31 @@ function App() {
             <th scope="col">País</th>
             <th scope="col">Cargo</th>
             <th scope="col">Experiencia</th>
+            <th scope="col">Acciones</th>
+
           </tr>
         </thead>
         <tbody>
           {
             empleados.map((val, key) => {
-              return <tr key={val.id }>
+              return <tr key={val.id}>
+                <td>{val.id} </td>
                 <td>{val.nombre} </td>
                 <td>{val.edad} </td>
-                <td>{val.pais} </td>  
+                <td>{val.pais} </td>
                 <td>{val.cargo} </td>
                 <td>{val.experiencia} años </td>
+                <td>
+                  <div className="btn-group" role="group" aria-label="Basic example">
+                    <button type="button"
+                      onClick={() => {
+                        editarEmpleado(val);
+                      }}
+                      className="btn btn-info" >Editar</button>
+                    <button type="button" className="btn btn-danger">Eliminar</button>
+                  </div>
+                </td>
+
               </tr>
             })
           }
